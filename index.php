13b9506e0e2d85d619203ad575db0a52a3d4c129
pdostal <git@pdostal.cz>
2013-12-30 17:07:24 +0100

    Add transparent border

--- index.php
+++ index.php
@@ -6,15 +6,11 @@
 		<?php $i = 1; if (have_posts()) { while (have_posts()) { the_post(); ?>
 		<?php if ($i == 4) { $i = 1; } ?>
 		<?php if ($i == 1) { ?><div class="rov"><?php } ?>
-			<div class="col-md-4">
-				<h1 class="title"><?php the_title(); ?></h1>
-				<div class="content"><?php the_content(); ?></div>
-				<div class="date"><?php the_time('j. n. Y') ?></div>
-				<div class="clear"></div>
-				<div class="category"><span class="desc">Kategorie: </span><?php the_category(', '); ?></div>
-				<div class="tags"><span class="desc">Štítky: </span><?php the_tags('', ', ', ''); ?></div>
-			</div>
-		<?php if ($i == 3) { ?></div><?php } ?>
+			<article class="col-md-4">
+				<h1 class="title"><?php the_time('j. n. Y') ?></h1>
+				<div class="content"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { ?><img src="" alt="No image" /><?php } ?></div>
+			</article>
+		<?php if ($i == 3) { ?></div><div class="clear"></div><?php } ?>
 		<?php $i++; } } if ($i !== 4) { ?></div><?php } ?>
 	</div>
 </section>
