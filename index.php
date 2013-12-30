bf653d9b4fca8f7231e83d902707b941e17d3b94
pdostal <git@pdostal.cz>
2013-12-30 15:14:25 +0100

    Prepare 3-col grid

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 	include('header.php');
 ?>
 <section id="main">
-<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-	<article class="container">
-		<h1 class="title"><?php the_title(); ?></h1>
-		<div class="content"><?php the_content(); ?></div>
-		<div class="date"><?php the_time('j. n. Y') ?></div>
-		<div class="clear"></div>
-		<div class="category"><span class="desc">Kategorie: </span><?php the_category(', '); ?></div>
-		<div class="tags"><span class="desc">Štítky: </span><?php the_tags('', ', ', ''); ?></div>
-	</article>
-<?php } } ?>
+	<div class="container">
+		<?php $i = 1; if (have_posts()) { while (have_posts()) { the_post(); ?>
+		<?php if ($i == 4) { $i = 1; } ?>
+		<?php if ($i == 1) { ?><div class="rov"><?php } ?>
+			<div class="col-md-4">
+				<h1 class="title"><?php the_title(); ?></h1>
+				<div class="content"><?php the_content(); ?></div>
+				<div class="date"><?php the_time('j. n. Y') ?></div>
+				<div class="clear"></div>
+				<div class="category"><span class="desc">Kategorie: </span><?php the_category(', '); ?></div>
+				<div class="tags"><span class="desc">Štítky: </span><?php the_tags('', ', ', ''); ?></div>
+			</div>
+		<?php if ($i == 3) { ?></div><?php } ?>
+		<?php $i++; } } if ($i !== 4) { ?></div><?php } ?>
+	</div>
 </section>
 <?php
 	include('footer.php');
--- single.php
+++ single.php
@@ -2,16 +2,16 @@
 	include('header.php');
 ?>
 <section id="main">
-<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-	<article class="container">
+	<article class="container single">
+		<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 		<h1 class="title"><?php the_title(); ?></h1>
 		<div class="content"><?php the_content(); ?></div>
 		<div class="date"><?php the_time('j. n. Y') ?></div>
 		<div class="clear"></div>
 		<div class="category"><span class="desc">Kategorie: </span><?php the_category(', '); ?></div>
 		<div class="tags"><span class="desc">Štítky: </span><?php the_tags('', ', ', ''); ?></div>
+		<?php } } ?>
 	</article>
-<?php } } ?>
 </section>
 <?php
 	include('footer.php');
