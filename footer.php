9c2096811a8bc8b4b71d467fd1c5092c1c258a6c
pdostal <git@pdostal.cz>
2013-12-31 01:55:51 +0100

    Show tags

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 		<footer class="container">
-			<span class="wordpressfooter">
-				Powered by <a href="http://wordpress.org/">Wordpress</a>
-			</span>
 			<span class="authorfooter">
 				&copy; <a itemprop="url" href="http://pdostal.cz/">Pavel Dostál</a>
 				(<a href="mailto:pdostal@pdostal.cz">pdostal@pdostal.cz</a>)
+			</span><br />
+			<span class="wordpressfooter">
+				Powered by <a href="http://wordpress.org/">Wordpress</a>
 			</span>
 		</footer>
 		<?php wp_footer(); ?>
--- single.php
+++ single.php
@@ -6,6 +6,7 @@
 		<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 			<h1 class="title"><?php the_time('j. n. Y') ?></h1>
 			<div class="content"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { echo get_first_image($post); } ?></div>
+			<div class="tags"><?php the_tags('Tags: #', ' #', ''); ?></div>
 		<?php } } ?>
 	</article>
 </section>
