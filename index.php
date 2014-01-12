5e1cddfb4ea3d5052bf952f2172f00465ea5b2cc
pdostal <git@pdostal.cz>
2014-01-12 22:37:25 +0100

    Add transparent box for text description

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 		<?php if ($i == 1) { ?><div class="rov"><?php } ?>
 			<article class="col-md-4">
 				<h1 class="title"><a href="<?php the_permalink(); ?>" itemprop="datePublished" content="<?php the_time('Y-m-d') ?>"><?php the_time('j. n. Y') ?></a></h1>
-				<div class="content"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { echo get_first_image($post); } ?></div>
+				<div class="content">
+					<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { echo get_first_image($post); } ?>
+					<div class="text" itemprop="description"><?php echo strip_tags(get_the_content()); ?></div>
+				</div>
 			</article>
 		<?php if ($i == 3) { ?></div><div class="clear"></div><?php } ?>
 		<?php $i++; } } if ($i !== 4) { ?></div><?php } ?>
--- single.php
+++ single.php
@@ -6,9 +6,11 @@
 		<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
 			<h1 class="title" itemprop="datePublished" content="<?php the_time('Y-m-d') ?>"><?php the_time('j. n. Y') ?></h1>
 			<div class="author" itemprop="author" style="display: none;"><?php echo get_the_author_firstname().' '.get_the_author_lastname(); ?></div>
-			<div class="content"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { echo get_first_image($post); } ?></div>
-			<div class="tags"><?php the_tags('Tags: #', ' #', ''); ?></div>
-			<div class="text" itemprop="description"><?php echo strip_tags(get_the_content()); ?></div>
+			<div class="content">
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } else { echo get_first_image($post); } ?>
+				<div class="text" itemprop="description"><?php echo strip_tags(get_the_content()); ?></div>
+			</div>
+			<!--<div class="tags"><?php the_tags('Tags: #', ' #', ''); ?></div>-->
 		<?php } } ?>
 	</article>
 </section>
