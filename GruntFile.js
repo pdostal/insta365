453bf6c0a29f3938c5dfd1fda4abc31c9f1b4f4c
pdostal <git@pdostal.cz>
2014-01-31 10:37:04 +0100

    Exclude package.json from minified version

--- GruntFile.js
+++ GruntFile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('./package.json'),
-    uglify: { dist: { files: { './insta365/main.js': ['./*.js'] } } },
-    cssmin: { dist: { files: { './insta365/main.css': ['./*.css'] } } },
+    uglify: { dist: { files: { './insta365/main.js': ['./main.js'] } } },
+    cssmin: { dist: { files: { './insta365/main.css': ['./main.css'] } } },
     copy: { dist: { files: [ { expand: true, dest: './insta365/', src: ['./*.png', './*.php', './ext/*', 'style.css'], cwd: './' } ] } },
     compress: { dist: { files: [ { src: ['./insta365/**'], dest: './' } ], options: { archive: 'insta365.zip' } } },
     remove: { dist: { dirList: ['./insta365/'], options: { trace: false } } }
